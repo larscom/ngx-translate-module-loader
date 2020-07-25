c3356d13deccd22edd71ddcd412415a32b2eb828
larscom <larskniep@gmail.com>
2020-07-25 13:38:42 +0200

    update to angular 10

--- projects/ngx-translate-module-loader/src/lib/module-translate-loader.ts
+++ projects/ngx-translate-module-loader/src/lib/module-translate-loader.ts
@@ -3,7 +3,7 @@ import { TranslateLoader } from '@ngx-translate/core';
 import merge from 'deepmerge';
 import { forkJoin as ForkJoin, MonoTypeOperatorFunction, Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-
+import { IModuleTranslation } from './models/module-translation';
 import { IModuleTranslationOptions } from './models/module-translation-options';
 import { Translation } from './models/translation';
 
@@ -18,7 +18,7 @@ export class ModuleTranslateLoader implements TranslateLoader {
     deepMerge: true,
     modulePathTemplate: '{baseTranslateUrl}/{moduleName}/{language}',
     pathTemplate: '{baseTranslateUrl}/{language}',
-    ...this.options
+    ...this.options,
   };
 
   /**
@@ -34,59 +34,16 @@ export class ModuleTranslateLoader implements TranslateLoader {
   constructor(private readonly http: HttpClient, private readonly options: IModuleTranslationOptions) {}
 
   public getTranslation(language: string): Observable<Translation> {
-    const {
-      deepMerge,
-      enableNamespacing,
-      nameSpaceUppercase,
-      modules,
-      translateError,
-      translateMerger,
-      pathTemplate,
-      modulePathTemplate
-    } = this.defaultOptions;
+    const { defaultOptions: options } = this;
+    return this.mergeTranslations(this.getModuleTranslations(language, options), options);
+  }
 
-    const moduleRequests = modules.map(({ baseTranslateUrl, moduleName, nameSpace, translateMap }) => {
-      if (!moduleName) {
-        const pathOptions = { baseTranslateUrl, language };
-        const path = createJsonPath(
-          pathTemplate.replace(PATH_TEMPLATE_MATCH, (_, m1: string) => pathOptions[m1] || '')
-        );
-        return this.http.get<Translation>(path).pipe(
-          map(translation => (translateMap ? translateMap(translation) : translation)),
-          this.catchError(path, translateError)
-        );
-      }
-
-      const modulePathOptions = { baseTranslateUrl, moduleName, language };
-      const modulePath = createJsonPath(
-        modulePathTemplate.replace(PATH_TEMPLATE_MATCH, (_, m1: string) => modulePathOptions[m1] || '')
-      );
-      return this.http.get<Translation>(modulePath).pipe(
-        map(translation => {
-          if (translateMap) {
-            return translateMap(translation);
-          }
-
-          if (!enableNamespacing) {
-            return translation;
-          }
-
-          const key = nameSpace
-            ? nameSpaceUppercase
-              ? nameSpace.toUpperCase()
-              : nameSpace.toLowerCase()
-            : nameSpaceUppercase
-            ? moduleName.toUpperCase()
-            : moduleName.toLowerCase();
-
-          return Object({ [key]: translation }) as Translation;
-        }),
-        this.catchError(modulePath, translateError)
-      );
-    });
-
-    return ForkJoin(moduleRequests).pipe(
-      map(translations => {
+  private mergeTranslations(
+    moduleTranslations: Observable<Translation>[],
+    { translateMerger, deepMerge }: IModuleTranslationOptions
+  ): Observable<Translation> {
+    return ForkJoin(moduleTranslations).pipe(
+      map((translations) => {
         if (translateMerger) {
           return translateMerger(translations);
         }
@@ -97,11 +54,64 @@ export class ModuleTranslateLoader implements TranslateLoader {
     );
   }
 
+  private getModuleTranslations(language: string, options: IModuleTranslationOptions): Observable<Translation>[] {
+    const { modules } = options;
+
+    return modules.map((module) => {
+      const { moduleName } = module;
+      return moduleName
+        ? this.fetchTranslationForModule(language, options, module)
+        : this.fetchTranslation(language, options, module);
+    });
+  }
+
+  private fetchTranslation(
+    language: string,
+    { pathTemplate, translateError }: IModuleTranslationOptions,
+    { baseTranslateUrl, translateMap }: IModuleTranslation
+  ): Observable<Translation> {
+    const pathOptions = { baseTranslateUrl, language };
+    const path = createJsonPath(pathTemplate.replace(PATH_TEMPLATE_MATCH, (_, m1: string) => pathOptions[m1] || ''));
+
+    return this.http.get<Translation>(path).pipe(
+      map((translation) => (translateMap ? translateMap(translation) : translation)),
+      this.catchError(path, translateError)
+    );
+  }
+  private fetchTranslationForModule(
+    language: string,
+    { modulePathTemplate, enableNamespacing, nameSpaceUppercase, translateError }: IModuleTranslationOptions,
+    { baseTranslateUrl, translateMap, moduleName, nameSpace }: IModuleTranslation
+  ): Observable<Translation> {
+    const modulePathOptions = { baseTranslateUrl, moduleName, language };
+    const modulePath = createJsonPath(
+      modulePathTemplate.replace(PATH_TEMPLATE_MATCH, (_, m1: string) => modulePathOptions[m1] || '')
+    );
+    const namespaceKey = nameSpace
+      ? nameSpaceUppercase
+        ? nameSpace.toUpperCase()
+        : nameSpace.toLowerCase()
+      : nameSpaceUppercase
+      ? moduleName.toUpperCase()
+      : moduleName.toLowerCase();
+
+    return this.http.get<Translation>(modulePath).pipe(
+      map((translation) => {
+        return translateMap
+          ? translateMap(translation)
+          : enableNamespacing
+          ? Object({ [namespaceKey]: translation })
+          : translation;
+      }),
+      this.catchError(modulePath, translateError)
+    );
+  }
+
   private catchError<T>(
     path: string,
     translateError?: (error: any, path: string) => void
   ): MonoTypeOperatorFunction<T> {
-    return catchError(e => {
+    return catchError((e) => {
       if (translateError) {
         translateError(e, path);
       }
